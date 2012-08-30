9f06e9a2a39c20666ad2f2c1c889dec75da5a573
Joao Gilberto Magalhaes <joao@byjg.com.br>
2012-08-30 14:54:59 +0000

    Added BaseModelCollection in the include file.

--- xmlnuke-php5/bin/database.inc.php
+++ xmlnuke-php5/bin/database.inc.php
@@ -1,6 +1,7 @@
 <?php 
 require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.basedbaccess.class.php");
 require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.basemodel.class.php");
+require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.basemodelcollection.class.php");
 require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.sqlhelper.class.php");
 require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.idbfunctions.class.php");
 require_once(PHPXMLNUKEDIR . "bin/com.xmlnuke/database.dbbasefunctions.class.php");
