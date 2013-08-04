4c90daf9d23c917a27fa6e7e705e888862ecb670
Joao Gilberto Magalhaes <joao@byjg.com.br>
2013-08-03 23:46:17 -0300

    Adjusts on main path; Ported "Create PHP5 Project" to native PHP script;

--- utils/packaging/xmlnuke-php5/makeit.sh
+++ utils/packaging/xmlnuke-php5/makeit.sh
@@ -6,7 +6,7 @@ TMPPATH="$1"
 BASE="/usr/local/xmlnuke"
 
 cp -R $TMPPATH/xmlnuke-php5 $BASE
-cp $TMPPATH/create-php5-project.sh $BASE
+cp $TMPPATH/create-php5-project.php $BASE
 cp $TMPPATH/create-php5-project /usr/bin
 chmod 755 /usr/bin/create-php5-project
 
