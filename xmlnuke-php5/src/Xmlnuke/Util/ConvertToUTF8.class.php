a8b9e3ac1bb3a55524eabfc47857cc4519fdeb00
Joao Gilberto Magalhaes <joao@byjg.com.br>
2013-10-08 17:29:50 -0300

    Continuing on PHP 5.3 migration

---
+++ xmlnuke-php5/src/Xmlnuke/Util/ConvertToUTF8.class.php
@@ -0,0 +1,71 @@
+<?php
+/*
+*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+*  Copyright:
+*
+*  XMLNuke: A Web Development Framework based on XML.
+*
+*  Main Specification: Joao Gilberto Magalhaes, joao at byjg dot com
+*  PHP Implementation: Joao Gilberto Magalhaes, joao at byjg dot com
+*
+*  This file is part of XMLNuke project. Visit http://www.xmlnuke.com
+*  for more information.
+*
+*  This program is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU General Public License
+*  as published by the Free Software Foundation; either version 2
+*  of the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+*/
+
+namespace Xmlnuke\Util;
+
+class ConvertToUTF8
+{
+	public static function FromHtmlEntities($text)
+	{
+		$HTML_ENTITIES = 
+			array
+			(
+				"&aacute;"=>161 /*á*/, "&eacute;"=>169 /*é*/, "&iacute;"=>173 /*í*/, "&oacute;"=>179 /*ó*/, "&uacute;"=>186 /*ú*/, 
+				"&agrave;"=>160 /*à*/, "&egrave;"=>168 /*è*/, "&igrave;"=>172 /*ì*/, "&ograve;"=>178 /*ò*/, "&ugrave;"=>185 /*ù*/, 
+				"&atilde;"=>163 /*ã*/, "&otilde;"=>181 /*õ*/, "&ntilde;"=>177 /*ñ*/, "&acirc;"=>162  /*â*/, "&ecirc;"=>170  /*ê*/, 
+				"&icirc;"=>174  /*î*/, "&ocirc;"=>180  /*ô*/, "&ucirc;"=>187  /*û*/, "&ccedil;"=>167 /*ç*/, "&Aacute;"=>129 /*Á*/, 
+				"&Eacute;"=>137 /*É*/, "&Iacute;"=>141 /*Í*/, "&Oacute;"=>147 /*Ó*/, "&Uacute;"=>154 /*Ú*/, "&Agrave;"=>128 /*À*/, 
+				"&Egrave;"=>136 /*È*/, "&Igrave;"=>140 /*Ì*/, "&Ograve;"=>146 /*Ò*/, "&Ugrave;"=>153 /*Ù*/, "&Atilde;"=>131 /*Ã*/, 
+				"&Otilde;"=>149 /*Õ*/, "&Ntilde;"=>145 /*Ñ*/, "&Acirc;"=>130  /*Â*/, "&Ecirc;"=>138  /*Ê*/, "&Icirc;"=>142  /*Î*/, 
+				"&Ocirc;"=>148  /*Ô*/, "&Ucirc;"=>155  /*Û*/, "&Ccedil;"=>135 /*Ç*/
+			);
+	
+		return ConvertToUTF8::BaseConversion($HTML_ENTITIES, $text);
+	}
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @param string[] $vector
+	 * @param string $text
+	 * @return string
+	 */
+	protected static function BaseConversion($vector, $text)
+	{
+		foreach ($vector as $key=>$value) 
+		{
+			$text = str_replace($key, chr(195).chr($value), $text);
+		}
+		
+		return $text;
+	}
+	
+}
