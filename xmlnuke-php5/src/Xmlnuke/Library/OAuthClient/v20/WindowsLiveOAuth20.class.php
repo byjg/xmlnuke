b902e6a7210c2b09c2c058ce24fb6840467fb336
Joao Gilberto Magalhaes <joao@byjg.com.br>
2014-09-15 22:35:48 -0300

    Fixed Windows Live and FourSquare OAuth client

--- xmlnuke-php5/src/Xmlnuke/Library/OAuthClient/v20/WindowsLiveOAuth20.class.php
+++ xmlnuke-php5/src/Xmlnuke/Library/OAuthClient/v20/WindowsLiveOAuth20.class.php
@@ -33,10 +33,6 @@ class WindowsLiveOAuth20 extends BaseOAuth20
 		}
 	}
 	
-	protected function preparedUrl($url) {
-		return parent::preparedUrl($this->GRAPH_API . $url);
-	}
-	
 	public function getData($objectId = "me", $params = null)
 	{
 		return json_decode($this->get(($objectId[0] != "/" ? "/" : "") . $objectId, $params));
