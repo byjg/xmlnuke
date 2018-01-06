d4ed2432c33d9ba1c0d1beb512b010de16578e7a
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-01-06 11:40:20 -0200

    Internationalization based on the library "INTL"

--- xmlnuke-php5/src/com.xmlnuke/international/international.cultureinfo.class.php
+++ xmlnuke-php5/src/com.xmlnuke/international/international.cultureinfo.class.php
@@ -30,16 +30,10 @@
 /** 
  * Get the Culture Info based in the context
  *
- * <b>Important Note</b>
- * This class is Operational System dependant. Your OS must support the desired language to get work
- * specific regional settings.
  *
- * On Debian machines:
- *   uncomment or add the desired language in file /etc/locale.gen (examples: pt_BR, en_CA, de_DE, etc)
- *   run locale-gen from the shell
- *
- * On Windows Machines:
- *   ??
+ * https://www.simonholywell.com/post/2015/07/international-php-dates-with-intl/
+ * https://www.sitepoint.com/localizing-dates-currency-and-numbers-with-php-intl/
+ * https://gist.github.com/bryanburgers/f375ea3086a0ed029636
  *
  * @package xmlnuke
  */
@@ -49,8 +43,6 @@ class CultureInfo
 	private $_Language;
 	private $_CharSet;
 	private $_cultureActive;
-
-	private $_localeConv;
 
 	/**
 	*@param
@@ -110,39 +102,6 @@ class CultureInfo
 		}
 		$this->_name = $language;
 		$systemLocale = $this->getIsoName();
-		$this->_cultureActive = setlocale(LC_ALL, $systemLocale . ".UTF8", $language . ".UTF-8", $systemLocale, $language, $this->_name);
-
-		// Try to load in Windows if failed before
-		$iLang = 0;
-		while (!$this->_cultureActive && ($iLang < sizeof($langstr)))
-		{
-			$this->_cultureActive = setlocale(LC_ALL, $langstr[$iLang]);
-			$iLang++;
-		}
-
-		if (!$this->_cultureActive)
-		{
-			$firstPart = substr ($language, 0, 2);
-			
-			if(FileUtil::isWindowsOS())
-				$complement = "";
-			else
-				$complement = "Only the languages available in 'locale -a' can be setted. On debian try execute 'apt-get install language-pack-$firstPart'.";
-
-			echo "<br/>\n<b>Warning: </b> The culture language '$language' was not found. $complement\n<br/>";
-
-			// I cant call the context here because it's call CultureInfo but the context is not complete yet.
-			//$context = Context::getInstance();
-			//$context->WriteWarningMessage ("");
-		}
-		
-		$this->_localeConv = localeConv();
-		#if (stripos(PHP_OS, 'win') !== false)
-		#{
-		#	$this->_localeConv["currency_symbol"] = utf8_encode($this->_localeConv["currency_symbol"]);
-		#}
-
-		#Debug::PrintValue($this->_localeConv, $langstr, $this->getRegionalMonthNames());
 	}
 
 	public function getIsoName()
@@ -156,16 +115,19 @@ class CultureInfo
 
 	public function getIntlCurrencySymbol()
 	{
-		return $this->_localeConv["int_curr_symbol"];
+        $numberFormat = new \NumberFormatter($this->_name, \NumberFormatter::CURRENCY);
+        return $numberFormat->getSymbol(\NumberFormatter::INTL_CURRENCY_SYMBOL);
 	}
 
 	public function getCurrencySymbol()
 	{
-		return $this->_localeConv["currency_symbol"];
+        $numberFormat = new \NumberFormatter($this->_name, \NumberFormatter::CURRENCY);
+        return $numberFormat->getSymbol(\NumberFormatter::CURRENCY_SYMBOL);
 	}
 	public function getDecimalPoint()
 	{
-		return $this->_localeConv["decimal_point"];
+        $a = new \NumberFormatter($this->_name, \NumberFormatter::DECIMAL_SEPARATOR_SYMBOL);
+        return $a->getSymbol(\NumberFormatter::DECIMAL_SEPARATOR_SYMBOL);
 	}
 
 
@@ -176,47 +138,31 @@ class CultureInfo
 
 	public function getRegionalMonthNames()
 	{
+        $f = new IntlDateFormatter($this->_name, null, null, null, null, 'MMMM');
+
 		$monthArray = array();
 		for ($i=1; $i<=12; $i++)
 		{
-			$monthArray[$i] = strftime("%B", mktime(0, 0, 0, $i, 1, 2009));
-			if (stripos(PHP_OS, 'win') !== false)
-			{
-				$monthArray[$i] = utf8_encode($monthArray[$i]);
-			}
+            $monthArray[$i] = $f->format(new DateTime("2018-$i-01"));
 		}
 		return $monthArray;
 	}
 
 	public function formatMoney($number, $intlSymbol = false, $truncate = false)
 	{
-		if (!is_numeric($number)) 
-			$number = doubleval (str_replace($this->_localeConv["decimal_point"], ".", $number));
+        $fmt = new NumberFormatter( $this->_name, $intlSymbol ? NumberFormatter::CURRENCY : NumberFormatter::DECIMAL);
 
-		if ($truncate)
-		{
-			$factor = pow(10, intval($this->_localeConv["frac_digits"] == "" || $this->_localeConv["frac_digits"] > 5 ? 2 : $this->_localeConv["frac_digits"]));
-			$number = intval($number * $factor) / $factor;
-		}
-		if (!$intlSymbol)
-		{
-			$format = "%01." . $this->_localeConv["frac_digits"] . "f";
-			$retorno = sprintf($format, $number);
+        if (!is_numeric($number)) {
+            $number = $this->getDoubleVal($number);
+        }
+        if ($truncate)
+        {
+            $numberFormatter->setAttribute(NumberFormatter::FRACTION_DIGITS, 2);
+            $numberFormatter->setAttribute(NumberFormatter::ROUNDING_MODE, NumberFormatter::ROUND_HALFEVEN);
+        }
 
-			$moneySymbol = $this->getCurrencySymbol();
-			if ($this->_localeConv["p_sep_by_space"]) $space = ' '; else $space = '';
-			$retorno = ($this->_localeConv["p_cs_precedes"] ? "$moneySymbol$space$retorno" : "$retorno$space$moneySymbol");
-		}
-		else
-		{
-			$format = "%01." . $this->_localeConv["frac_digits"] . "F";
-			$retorno = sprintf($format, $number);
-
-			$retorno = $retorno . " " . $this->getIntlCurrencySymbol();
-		}
-
-		return $retorno;
-	}
+        return $fmt->format($number);
+    }
 
 	/**
 	 * Enter description here...
@@ -225,8 +171,19 @@ class CultureInfo
 	 */
 	public function getDateFormat()
 	{
-		$date = strftime("%x", mktime(0, 0, 0, 1, 31, 2009));
-		if (preg_match("/31[- \/.]0?1[- \/.](20)?09/", $date))
+        $fmt = new IntlDateFormatter(
+            $this->_name,
+            IntlDateFormatter::SHORT,
+            IntlDateFormatter::NONE
+        );
+        $pattern = $fmt->getPattern();
+        $pattern = preg_replace(
+            '/(?<!y)yy(?!y)/',
+            'yyyy',
+            $pattern);
+        $fmt->setPattern($pattern);
+        $date = $fmt->format(new DateTime('2018-01-31'));
+		if (preg_match("/31[- \/.]0?1[- \/.](20)?18/", $date))
 		{
 			return DATEFORMAT::DMY;
 		}
@@ -240,9 +197,13 @@ class CultureInfo
 		}
 	}
 
-	public function getDoubleVal($value)
+	public function getDoubleVal($value = 0)
 	{
-		return doubleval(str_replace($this->getDecimalPoint(), ".", $value));
+		if (doubleval($value) === $value) {
+			return $value;
+		}
+        $fmt = new NumberFormatter( $this->_name, NumberFormatter::DECIMAL );
+        return $fmt->parse($value);
 	}
 
 }
